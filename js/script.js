0ce08133b7580e49387def7543594c1571de34b7
Nit-Harley <harleynit63@gmail.com>
2023-12-15 17:48:28 +0100

    Site de test pour un stage

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+
+ const filterContainer = document.querySelector(".gallery-filter"),
+ galleryItems = document.querySelectorAll(".gallery-item");
+
+ filterContainer.addEventListener("click", (event) =>{
+   if(event.target.classList.contains("filter-item")){
+   	 // désactiver les filtres actifs 'filter-item'
+   	 filterContainer.querySelector(".active").classList.remove("active");
+   	 // activer nouveau 'filter-item'
+   	 event.target.classList.add("active");
+   	 const filterValue = event.target.getAttribute("data-filter");
+   	 galleryItems.forEach((item) =>{
+       if(item.classList.contains(filterValue) || filterValue === 'all'){
+       	item.classList.remove("hide");
+       	 item.classList.add("show");
+       }
+       else{
+       	item.classList.remove("show");
+       	item.classList.add("hide");
+       }
+   	 });
+   }
+ });
